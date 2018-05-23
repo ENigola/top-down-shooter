26c4a4c847bbcb1a980d93f66b98d049f50f9946
Ergo Nigola <ergo.nigola@gmail.com>
2018-05-23 23:53:54 +0300

    More input handling

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
+	private int currentWeapon;
 	public float moveSpeed;
 	
 	void Update () {
-		transform.Translate(new Vector2(moveSpeed, 0) * Input.GetAxis("Horizontal") * Time.deltaTime);
-		transform.Translate(new Vector2(0, moveSpeed) * Input.GetAxis("Vertical") * Time.deltaTime);
+		// Movement
+		transform.Translate(new Vector2(moveSpeed, 0) * Input.GetAxis("Horizontal") * Time.deltaTime, Space.World);
+		transform.Translate(new Vector2(0, moveSpeed) * Input.GetAxis("Vertical") * Time.deltaTime, Space.World);
+		// Looking direction
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 lookDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+		transform.right = lookDirection;
+		// Weapon switching
+		if (Input.GetAxis("Select 1") != 0f) {
+			currentWeapon = 0;
+		} else if (Input.GetAxis("Select 2") != 0f) {
+			currentWeapon = 1;
+		} else if (Input.GetAxis("Select 3") != 0f) {
+			currentWeapon = 2;
+		}
+		int i = 0;
+		foreach (Transform child in transform) {
+			if (currentWeapon == i) {
+				child.gameObject.SetActive(true);
+			} else {
+				child.gameObject.SetActive(false);
+			}
+			i++;
+		}
 	}
 }
