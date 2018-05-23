cd03ddb850ebf500b400e17963b2a9ade38fd9a0
Ergo Nigola <ergo.nigola@gmail.com>
2018-05-23 19:15:57 +0300

    Basic setup and art.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	private float cameraOffsetZ = -10;
+
+	void Start () {
+	}
+	
+	void Update () {
+		Camera.main.transform.position = transform.position + new Vector3(0, 0, cameraOffsetZ);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed;
+	
+	void Update () {
+		transform.Translate(new Vector2(moveSpeed, 0) * Input.GetAxis("Horizontal") * Time.deltaTime);
+		transform.Translate(new Vector2(0, moveSpeed) * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
